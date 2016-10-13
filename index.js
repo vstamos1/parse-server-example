033131a61ed750aabfa15a68e2f841b05187a983
Vincent Stamos <vstamos1@yahoo.com>
2016-10-13 23:54:50 +0200

    added liveQueries2

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  serverURL: "http://notificationsvjs.herokuapp.com/parse",
-  databaseURI: databaseUri || 'mongodb://heroku_9kqnwqfv:3feokrd5vo5vs013fd6v945frk@ds035766.mlab.com:35766/heroku_9kqnwqfv',
+  serverURL: "",//http://notificationsvjs.herokuapp.com/parse
+  databaseURI: databaseUri || '',//mongodb://heroku_9kqnwqfv:3feokrd5vo5vs013fd6v945frk@ds035766.mlab.com:35766/heroku_9kqnwqfv
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppNotificationsId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey123123', //Add your master key here. Keep it secret!
-  liveQuery: {
-    classNames: ['Events', 'Messages']
-  }
+  appId: process.env.APP_ID || '', //myAppNotificationsId
+  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!   ,myMasterKey123123
+  // liveQuery: {
+//     classNames: ['Events', 'Messages']
+//   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -36,7 +36,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a web site.');
+  res.status(200).send('I dream of being a fish.');
 });
 
 var port = process.env.PORT || 1337;
